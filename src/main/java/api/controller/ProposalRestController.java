5e839339b6013735d72f1120657e5fd1574f25d9
spoint00 <filipe.pt567@gmail.com>
2024-04-09 18:41:11 +0100

    cross-origins

--- src/main/java/api/controller/ProposalRestController.java
+++ src/main/java/api/controller/ProposalRestController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+@CrossOrigin(origins = "http://localhost:8180")
 @RestController
 @RequestMapping("/proposals")
 public class ProposalRestController {
--- src/main/java/api/controller/StudentRestController.java
+++ src/main/java/api/controller/StudentRestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-//@CrossOrigin(origins = "http://localhost:8180")
+@CrossOrigin(origins = "http://localhost:8180")
 @RestController
 @RequestMapping("/students")
 public class StudentRestController {
